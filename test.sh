350a0e3dc02c7b18d9b907f2d959fdad7e9dd18e
Kirill Zaborsky <qrilka@gmail.com>
2019-02-15 13:15:00 +0300

    Typo

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ for i in $(seq 1 500); do
    sed -E 's/(id: sample-1.0.0)-inplace/\1-x'"$i"'/' -i "test-db/sample-1.0.0-$i.conf";
 done
 ghc-pkg --package-db=test-db recache
-echo "Unregistring single package:"
+echo "Unregistering single package:"
 time ghc-pkg --package-db=test-db unregister --ipid sample-1.0.0-x1
-echo "Unregistring 15 packages:"
+echo "Unregistering 15 packages:"
 time ghc-pkg --package-db=test-db unregister --ipid sample-1.0.0-x2 --ipid sample-1.0.0-x3 --ipid sample-1.0.0-x4 --ipid sample-1.0.0-x5 --ipid sample-1.0.0-x6 --ipid sample-1.0.0-x7 --ipid sample-1.0.0-x8 --ipid sample-1.0.0-x9 --ipid sample-1.0.0-x10 --ipid sample-1.0.0-x11 --ipid sample-1.0.0-x12 --ipid sample-1.0.0-x13 --ipid sample-1.0.0-x14 --ipid sample-1.0.0-x15 --ipid sample-1.0.0-x16
